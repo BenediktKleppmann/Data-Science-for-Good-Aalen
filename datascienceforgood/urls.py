0ceb07f18e33751dbfcf79af9a3e6c65cd7a41b2
owner <benedikt@kleppmann.de>
2019-08-17 18:04:08 +0200

    adding favicon.ico

--- datascienceforgood/urls.py
+++ datascienceforgood/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # THE WEBSITE  
     # ==================================================
     url(r'^$', views.home, name='home'),
+    url(r'^/$', views.home, name='home'),
     url(r'^analyse_12_09_2019/$', views.analyse_12_09_2019, name='analyse_12_09_2019'),
     url(r'^analyse_26_09_2019/$', views.analyse_26_09_2019, name='analyse_26_09_2019'),
     url(r'^analyse_10_10_2019/$', views.analyse_10_10_2019, name='analyse_10_10_2019'),
